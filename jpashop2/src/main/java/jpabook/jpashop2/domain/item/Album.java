e0a8c86b951c67e43f65f21e0d459ae6d465f75a
kjkj2089 <62068368+kjkj2089@users.noreply.github.com>
2023-02-04 00:24:53 +0900

    도메인 분석 설계- 엔티티 클래스 개발2

--- jpashop2/src/main/java/jpabook/jpashop2/domain/item/Album.java
+++ jpashop2/src/main/java/jpabook/jpashop2/domain/item/Album.java
@@ -7,7 +7,7 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("B")
+@DiscriminatorValue("A")
 @Getter
 @Setter
 
--- jpashop2/src/main/java/jpabook/jpashop2/domain/item/Movie.java
+++ jpashop2/src/main/java/jpabook/jpashop2/domain/item/Movie.java
@@ -7,7 +7,7 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
-@DiscriminatorValue("B")
+@DiscriminatorValue("M")
 @Getter
 @Setter
 
